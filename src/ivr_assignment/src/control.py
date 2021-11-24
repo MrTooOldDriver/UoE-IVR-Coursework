1122163931fa2e0b07dbe73a06c4fc7c361f9a86
ljacksonpan <ljacksonpan@outlook.com>
2021-11-24 01:50:48 +0000

    FK homogeneous matrix fix

--- src/ivr_assignment/src/control.py
+++ src/ivr_assignment/src/control.py
@@ -4,7 +4,11 @@ import numpy as np
 # import rospy
 import cv2
 import numpy as np
+from sympy import sin, cos, Matrix
+from sympy.abc import alpha, beta, gamma
 import zmq.utils.constant_names
+
+
 # from std_msgs.msg import Float64MultiArray, Float64
 #
 # # initialize a publisher for end effector target positions
@@ -20,7 +24,7 @@ import zmq.utils.constant_names
 #     d = 0
 
 
-def homogeneous_mat(q):
+def get_homogeneous_mat(q):
 
     As = []
 
@@ -28,21 +32,28 @@ def homogeneous_mat(q):
     As.append(np.array(
         [[np.cos(q[0]), -np.sin(q[0]), 0, 0],
          [np.sin(q[0]), np.cos(q[0]), 0, 0],
-         [0, 0, 1, 4],
+         [0, 0, 1, 0],
          [0, 0, 0, 1]]
     ))
     # Rotation on x axis
     As.append(np.array(
         [[1, 0, 0, 0],
          [0, np.cos(q[2]), -np.sin(q[2]), 0],
-         [0, np.sin(q[2]), np.cos(q[2]), 3.2],
+         [0, np.sin(q[2]), np.cos(q[2]), 4],
          [0, 0, 0, 1]]
     ))
     # Rotation on y axis
     As.append(np.array(
         [[np.cos(q[3]), 0, np.sin(q[3]), 0],
          [0, 1, 0, 0],
-         [-np.sin(q[3]), 0, np.cos(q[3]), 2.8],
+         [-np.sin(q[3]), 0, np.cos(q[3]), 3.2],
+         [0, 0, 0, 1]]
+    ))
+    # Move to end effector
+    As.append(np.array(
+        [[1, 0, 0, 0],
+         [0, 1, 0, 0],
+         [0, 0, 1, 2.8],
          [0, 0, 0, 1]]
     ))
 
@@ -53,10 +64,20 @@ def homogeneous_mat(q):
     return H
 
 
+def get_jacobian():
+    # H = Matrix([[cos(alpha) * cos(gamma) - sin(alpha) * sin(beta) * sin(gamma), -sin(alpha) * cos(beta),
+    #              cos(alpha) * sin(gamma) + sin(alpha) * sin(beta) * cos(gamma), 2.8 * sin(alpha) * sin(beta)],
+    #             [sin(alpha) * cos(gamma) + cos(alpha) * sin(beta) * sin(gamma), cos(alpha) * cos(beta),
+    #              sin(alpha) * sin(gamma) - cos(alpha) * sin(beta) * cos(gamma), -2.8 * cos(alpha) * sin(beta)],
+    #             [-cos(beta) * sin(gamma), sin(beta), cos(beta) * cos(gamma), 2.8 * cos(beta) + 7.2],
+    #             [0, 0, 0, 1]])
+    H = Matrix([2.8 * sin(alpha) * sin(beta), -2.8 * cos(alpha) * sin(beta), 2.8 * cos(beta) + 7.2])
+    return H.jacobian(Matrix([alpha, beta, gamma]))
+
 
 # For testing purposes
 if __name__ == '__main__':
     q = [1, 1, 1, 1]
-    H = homogeneous_mat(q)
+    H = get_homogeneous_mat(q)
     print(H)
-
+    print(get_jacobian())
