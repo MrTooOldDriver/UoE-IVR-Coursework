22543f1f7b1fcdc189dee40132fc1f1b1581dfe1
ljacksonpan <ljacksonpan@outlook.com>
2021-11-26 04:17:38 +0000

    completed IK

--- src/ivr_assignment/src/control.py
+++ src/ivr_assignment/src/control.py
@@ -27,7 +27,6 @@ from target import target_publisher
 #     d = 0
 
 def open_control():
-
     # Defines publisher and subscriber
     # initialize the node named
     rospy.init_node('open_control', anonymous=True)
@@ -42,7 +41,6 @@ def open_control():
     t1 = rospy.get_time()
 
     while not rospy.is_shutdown():
-
         target_pos = rospy.Subscriber("target_pos", Float64MultiArray, target_publisher)
         angle_1 = rospy.Subscriber("joint_angle_1", Float64MultiArray, )
         angle_3 = rospy.Subscriber("joint_angle_3", Float64MultiArray, )
@@ -52,9 +50,11 @@ def open_control():
         dt = cur_time - t1
         t1 = cur_time
 
-        cur_pos = get_end_effector_pos()
+        q = [angle_1, angle_3, angle_4]
+        cur_pos = get_end_effector_pos(q)
+
         err = target_pos - cur_pos
-        q_d = get_ik_angles([angle_1, angle_3, angle_4], err, dt)
+        q_d = get_ik_angles(q, err, dt)
 
         robot_joint1_pub.publish(Float64(q_d[0]))
         robot_joint3_pub.publish(Float64(q_d[1]))
@@ -62,8 +62,7 @@ def open_control():
         rate.sleep()
 
 
-def forward_kinematics(q):
-
+def forward_kinematics():
     # Defines publisher and subscriber
     # initialize the node named
     rospy.init_node('forward_kinematics', anonymous=True)
@@ -71,22 +70,25 @@ def forward_kinematics(q):
 
     # initialize publishers
 
-    robot_joint1_pub = rospy.Publisher("/robot/joint1_position_controller/command", Float64, queue_size=10)
-    robot_joint3_pub = rospy.Publisher("/robot/joint3_position_controller/command", Float64, queue_size=10)
-    robot_joint4_pub = rospy.Publisher("/robot/joint4_position_controller/command", Float64, queue_size=10)
+    # robot_joint1_pub = rospy.Publisher("/robot/joint1_position_controller/command", Float64, queue_size=10)
+    # robot_joint3_pub = rospy.Publisher("/robot/joint3_position_controller/command", Float64, queue_size=10)
+    # robot_joint4_pub = rospy.Publisher("/robot/joint4_position_controller/command", Float64, queue_size=10)
 
-    while not rospy.is_shutdown():
-        robot_joint1_pub.publish(Float64(q[0]))
-        robot_joint3_pub.publish(Float64(q[1]))
-        robot_joint4_pub.publish(Float64(q[2]))
+    angle_1 = rospy.Subscriber("joint_angle_1", Float64MultiArray, )
+    angle_3 = rospy.Subscriber("joint_angle_3", Float64MultiArray, )
+    angle_4 = rospy.Subscriber("joint_angle_4", Float64MultiArray, )
+
+    q = [angle_1, angle_3, angle_4]
+    cur_pos = get_end_effector_pos(q)
+
+    # TODO: Output calculation results and plot table to verify
 
 
-def get_end_effector_pos():
-    return [1, 1, 1]
+def get_end_effector_pos(q):
+    return get_homogeneous_mat(q)[:3, 3]
 
 
 def get_homogeneous_mat(q):
-
     As = [np.array(
         [[np.cos(q[0]), -np.sin(q[0]), 0, 0], [np.sin(q[0]), np.cos(q[0]), 0, 0],
          [0, 0, 1, 0], [0, 0, 0, 1]]
@@ -143,21 +145,19 @@ def get_ik_angles(q, err, dt):
     return q_d
 
 
-OPEN_LOOP = False
+OPEN_LOOP = True
 
 # run the code if the node is called
 if __name__ == '__main__':
     try:
-        # Switch between FK and IK
+        # Switch between Q1 and Q2
         if OPEN_LOOP:
             open_control()
         else:
-            # Set angles to test
-            forward_kinematics([0.32, 1.234, 0.123])
+            # To verify FK calculations and plot
+            forward_kinematics()
     except rospy.ROSInterruptException:
         pass
-
-
 
 # # For testing purposes
 # if __name__ == '__main__':
