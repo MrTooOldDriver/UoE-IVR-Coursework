0e61b2f6072d5f80405570b0f3630d8a197a772b
ljacksonpan <ljacksonpan@outlook.com>
2021-12-03 00:49:24 +0000

    cleaned code

--- src/ivr_assignment/src/control.py
+++ src/ivr_assignment/src/control.py
@@ -1,36 +1,20 @@
 #!/usr/bin/env python3
 
-import numpy
-import numpy as np
-import roslib
-import sys
 import rospy
-import cv2
 import numpy as np
 from sympy import sin, cos, Matrix
 from sympy.abc import alpha, beta, gamma
-import zmq.utils.constant_names
 from std_msgs.msg import Float64MultiArray, Float64
-from target import target_publisher
-
-
-# # initialize a publisher for end effector target positions
-# joint2_cmd_pub = rospy.Publisher("/robot/joint2_position_controller/command", Float64, queue_size=10)
-# joint3_cmd_pub = rospy.Publisher("/robot/joint3_position_controller/command", Float64, queue_size=10)
-# joint4_cmd_pub = rospy.Publisher("/robot/joint4_position_controller/command", Float64, queue_size=10)
-
-
-# class Q:
-#     theta = 0
-#     alpha = 0
-#     a = 0
-#     d = 0
 
 
 def get_homogeneous_mat_q21(q):
+
+    # This function was written for question 2.1
+
     # Rotation on y axis
     # Rotation on x axis
     # Transform to the blue
+
     As = [np.array(
         [[np.cos(q[0]), 0, np.sin(q[0]), 0], [0, 1, 0, 0],
          [-np.sin(q[0]), 0, np.cos(q[0]), 0], [0, 0, 0, 1]]
@@ -57,6 +41,7 @@ def get_homogeneous_mat(q):
     # Rotation on x axis
     # Rotation on y axis
     # Transform to the end effector
+
     As = [np.array(
         [[np.cos(q[0]), -np.sin(q[0]), 0, 0], [np.sin(q[0]), np.cos(q[0]), 0, 0],
          [0, 0, 1, 0], [0, 0, 0, 1]]
@@ -81,22 +66,15 @@ def get_homogeneous_mat(q):
 
 
 def get_jacobian(q):
-    # H = Matrix([[cos(alpha) * cos(gamma) - sin(alpha) * sin(beta) * sin(gamma), -sin(alpha) * cos(beta),
-    #              cos(alpha) * sin(gamma) + sin(alpha) * sin(beta) * cos(gamma), 2.8 * sin(alpha) * sin(beta)],
-    #             [sin(alpha) * cos(gamma) + cos(alpha) * sin(beta) * sin(gamma), cos(alpha) * cos(beta),
-    #              sin(alpha) * sin(gamma) - cos(alpha) * sin(beta) * cos(gamma), -2.8 * cos(alpha) * sin(beta)],
-    #             [-cos(beta) * sin(gamma), sin(beta), cos(beta) * cos(gamma), 2.8 * cos(beta) + 7.2],
-    #             [0, 0, 0, 1]])
 
     # The 3-d location coordinate vector from homogeneous
     O = Matrix(
         [3.2 * sin(alpha) * sin(beta) + 2.8 * (cos(alpha) * sin(gamma) + sin(alpha) * sin(beta) * cos(gamma)),
          2.8 * (sin(alpha) * sin(gamma) - cos(alpha) * sin(beta) * cos(gamma)) - 3.2 * cos(alpha) * sin(beta),
          2.8 * cos(beta) * cos(gamma) + 3.2 * cos(beta) + 4])
+
     # Get jacobian matirx using sympy
     J = O.jacobian(Matrix([alpha, beta, gamma]))
-    # print(J)
-    # print("_________________________________")
     return np.matrix(J.subs([(alpha, q[0]), (beta, q[1]), (gamma, q[2])]).evalf(), dtype='float')
 
 
@@ -105,15 +83,13 @@ def get_end_effector_pos(q):
 
 
 def get_pseudo_inverse(J):
-    return np.linalg.inv(J)
-    # return np.matmul(J.T, np.linalg.inv(np.matmul(J, J.T)))
+    return np.matmul(J.T, np.linalg.inv(np.matmul(J, J.T)))
 
 
-def get_ik_angles(q, err, dt):
+def get_target_angles(q, err, dt):
     J = get_jacobian(q)
     J_inv = get_pseudo_inverse(J)
     q_d = q + (dt * np.dot(J_inv, err.T))
-    # print(q_d)
     return q_d
 
 
@@ -135,7 +111,7 @@ class Control:
         # Defines publisher and subscriber
         # initialize the node named
         rospy.init_node('control', anonymous=True)
-        # rate = rospy.Rate(50)  # 50hz
+        self.rate = rospy.Rate(50)  # 50hz
         self.t1 = rospy.get_time()
 
         self.joint1 = 0
@@ -166,11 +142,13 @@ class Control:
         self.end_effector_sub = rospy.Subscriber("end_effector", Float64MultiArray, self.end_effector_callback)
 
     def forward_kinematics(self):
+
         # To observe & verify that FK is working properly
 
         # Use angles read from topics
         q = [self.joint1, self.joint3, self.joint4]
-        # Or set angles
+
+        # Or set different angles manually (output 10 sets by hand)
         q = [0.6, 0.7, 0.9]
 
         self.robot_joint1_pub.publish(Float64(q[0]))
@@ -183,18 +161,18 @@ class Control:
         # print("Calculated by FK: " + str(cur_pos))
         # print("Estimation from image: " + str(self.end_effector))
 
-    def open_control(self):
+    def inverse_kinematics(self):
         try:
+
             cur_time = rospy.get_time()
             dt = cur_time - self.t1
-            self.t1 = cur_time
 
             q = [self.joint1, self.joint3, self.joint4]
             cur_pos = get_end_effector_pos(q)
 
             target_pos = np.array(self.target_pos)
             err = target_pos - cur_pos
-            q_d = get_ik_angles(q, err, dt)
+            q_d = get_target_angles(q, err, dt)
 
             print("Error from target: " + str(err))
             q_d = np.squeeze(np.asarray(q_d))
@@ -209,13 +187,19 @@ class Control:
             self.tar_ef_y_pub.publish(Float64(target_pos[1]))
             self.true_ef_z_pub.publish(Float64(cur_pos[2]))
             self.tar_ef_z_pub.publish(Float64(target_pos[2]))
+
+            self.t1 = cur_time
+
+            for i in range(2):
+                self.rate.sleep()
+
         except:
             pass
 
     def control_main(self):
         # Switch between Q1 (FK) and Q2 (IK)
         if self.OPEN_LOOP:
-            self.open_control()
+            self.inverse_kinematics()
         else:
             self.forward_kinematics()
 
@@ -248,11 +232,4 @@ if __name__ == '__main__':
     except rospy.ROSInterruptException:
         print("Shutting down control")
 
-# # For testing purposes
-# if __name__ == '__main__':
-#     q1 = [1, 1, 1]
-#     H = get_homogeneous_mat(q1)
-#     print(H[:3, 3])
-#     print(get_jacobian(q1))
-#     J_p = get_ik_angles(q1, H[:3, 3])
-#     print(J_p)
+
